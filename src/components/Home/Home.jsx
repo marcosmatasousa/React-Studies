5a3bdb532fd1c4d51ed8c437fd4db038229d0102
marcosmatasousa <marcosdamata2000@gmail.com>
2024-09-17 14:42:08 -0300

    Implemented a route system

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+import './styles.css'
+
+function Home() {
+    return(
+        <ul>
+            <li>
+                <Link className="home-link" to="/react-facts">ReactFacts</Link>
+            </li>
+            <li>
+                <Link className="home-link" to="/business">BusinessCard</Link>
+            </li>
+            <li>
+                <Link className="home-link" to="/airbnb">AirBnbCard</Link>
+            </li>
+            <li>
+                <Link className="home-link" to="/travel-journey">TravelJourney</Link>
+            </li>
+            <li>
+                <Link className="home-link" to="/meme-generator">MemeGenerator</Link>
+            </li>
+        </ul>
+    )
+}
+
+export default Home;
