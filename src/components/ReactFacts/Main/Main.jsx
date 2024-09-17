cb438a09627a390ca483a1f817ac1ef7b1965559
marcosmatasousa <marcosdamata2000@gmail.com>
2024-09-17 14:41:24 -0300

    Added some styling changes

--- src/components/ReactFacts/Main/Main.jsx
+++ src/components/ReactFacts/Main/Main.jsx
@@ -15,7 +15,7 @@ function Main(props){
             <h1>Fun facts about React</h1>
             <ul>
                 <li>Was first released in 2013</li>
-                <li>Was originalli created by Jordan Walke</li>
+                <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 200K stars on GitHub</li>
                 <li>Is maintained by Meta</li>
                 <li>Power thousands of enterprise apps, including mobile apps</li>
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,6 @@
 import { createRoot } from 'react-dom/client'
 
-import App from './components/App';
-import AirBnbExp from './components/AirBnbExp/AirBnbExp';
-import BusinessCard from './components/BusinessCard/BusinessCard';
-import ReactFacts from './components/ReactFacts/ReactFacts';
-import TravelJourney from './components/TravelJourney/TravelJourney';
-import MemeGenerator from './components/MemeGenerator/MemeGenerator';
-import SignUp from './components/SignUp/SignUp';
-
+import App from './components/App'
 
 import './index.css'
 
@@ -16,6 +9,6 @@ const root = createRoot(rootElement);
 
 root.render(
     <div className='display'>
-      <ReactFacts />
+      <App />
     </div>
 );
